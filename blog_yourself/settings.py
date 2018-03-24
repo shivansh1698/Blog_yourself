136de014a2e878b52dc9a7773d8baaa6a6eee17d
shivansh1698 <shivansh.1698@gmail.com>
2018-03-24 20:40:30 +0530

    modified settings

--- blog_yourself/settings.py
+++ blog_yourself/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'qyuu3xah4rw4lt@m#jy=b60j@1hw_77^o2h=j9yci%_5dx3w+5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = ['shivansh1698.pythonanywhere',
+
 ]
 
 
